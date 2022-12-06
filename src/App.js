58ba935ff2aab756101102f5d5dd18ed068955f5
Deimar <deimarparn14@gmail.com>
2022-12-06 22:47:52 +0200

    Implemented countdown function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,26 @@ import instagram from './images/icon-instagram.svg'
 import pinterest from './images/icon-pinterest.svg'
 
 function App() {
+
+  let countDownDate = new Date("Jan 1, 2023 00:00:00").getTime();
+
+  let udate = setInterval(function() {
+    
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+
+  let days =  Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  }, 1000);
+
   return (
     <body style={{
       backgroundImage: `url(${stars})` 
@@ -14,19 +34,19 @@ function App() {
     <div className="body-container">
     <h1>We are launching soon</h1>
     <div class="grid-container">
-      <div class="grid-item">1</div>
-      <div class="grid-item">2</div>
-      <div class="grid-item">3</div>  
-      <div class="grid-item">4</div>
+      <div class="grid-item" id="days"></div>
+      <div class="grid-item" id="hours"></div>
+      <div class="grid-item" id="minutes"></div>  
+      <div class="grid-item" id="seconds"></div>
     </div>
     </div>
     <div className="bottom-container" style={{
       backgroundImage: `url(${hills})`,
     }}>
       <div className="social-icons">
-      <img src={facebook} />
-      <img src={pinterest} />
-      <img src={instagram} />
+        <img src={facebook} />
+        <img src={pinterest} />
+        <img src={instagram} />
       </div>
     </div>
    </div>
